1b14cd17aec73ff6d6080b91eecec0e2956cd4fc
Rafael-Cirino <r223730@dac.unicamp.br>
2023-08-31 12:03:35 -0300

    added delay on moment of read sensor

--- pluviometro_V0/pluviometro_V0.ino
+++ pluviometro_V0/pluviometro_V0.ino
@@ -1,19 +1,19 @@
+int ler_sensor_anterior;
 boolean ler_sensor;
 int conta_virada_pote;
-
-void interrupt_3() {
-  conta_virada_pote = (conta_virada_pote + 1);
-  Serial.println(conta_virada_pote);
-}
-
 
 void setup() {
   Serial.begin(9600);
   pinMode(3, INPUT);
-  attachInterrupt(digitalPinToInterrupt(3), interrupt_3, CHANGE);
+  ler_sensor_anterior = 0;
 }
 
 void loop() {
-  //ler_sensor = digitalRead(3);
-  //Serial.println(ler_sensor);
+  ler_sensor = digitalRead(3);
+  if (ler_sensor != ler_sensor_anterior) {
+    conta_virada_pote = conta_virada_pote + 1;
+    Serial.println(conta_virada_pote);
+    delay(2 * 1000);
+    ler_sensor_anterior = ler_sensor;
+  }
 }
