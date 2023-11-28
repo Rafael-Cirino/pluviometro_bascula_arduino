d3ee6ed6c15359194e992e49efeab5f5faecabce
Rafael-Cirino <r223730@dac.unicamp.br>
2023-11-27 21:32:31 -0300

    Uploading final codes

--- ler_memoria/ler_memoria.ino
+++ ler_memoria/ler_memoria.ino
@@ -7,6 +7,7 @@ int ler_sensor_anterior = 0;
 int conta_virada_pote = 0;
 float ml_por_virada = 2.52;
 int ultimo_endereco = 0;
+int entrada_usuario = 0;
 float m3_medido = 0;
 float mm_chuva = 0;
 
@@ -14,30 +15,37 @@ float mm_chuva = 0;
 
 void calc_chuva() {
   m3_medido = conta_virada_pote * ml_por_virada;
-  mm_chuva = m3_medido / pow(10, 3);
-}
-
-void print_chuva() {
-  Serial.print("Viradas de copo: ");
-  Serial.print(conta_virada_pote);
-  Serial.print(" | Qtd de chuva: ");
-  Serial.print(mm_chuva);
-  Serial.println(" mm");
+  mm_chuva = m3_medido * (127.39 / pow(10, 3));
 }
 
 
 void setup() {
   Serial.begin(9600);
-  ler_sensor_anterior = 0;
   ultimo_endereco = int(EEPROM.read(511)) + int(EEPROM.read(512));
+  Serial.println("");
+  Serial.println("Digite 1: Para ler os dados");
+  Serial.println("Digite 2: Para apagar todos os dados da memória");
 }
 
 void loop() {
-  for (int i = 0; i <= ultimo_endereco; i = i + 1) {
-    conta_virada_pote = int(EEPROM.read(i));
-    calc_chuva();
-    Serial.print("mm_chuva:");
-    Serial.println(mm_chuva);
+  if (Serial.available() > 0) {
+    entrada_usuario = Serial.read() - 48;
   }
-  ultimo_endereco = -1;
+  if (entrada_usuario == 1) {
+    for (int i = 0; i <= ultimo_endereco; i = i + 1) {
+      conta_virada_pote = int(EEPROM.read(i));
+      calc_chuva();
+      Serial.print("mm_chuva:");
+      Serial.println(mm_chuva);
+    }
+    entrada_usuario = 0;
+  }
+  if (entrada_usuario == 2) {
+    for (int i = 0; i <= 512; i = i + 1) {
+      EEPROM.write(i, 0);
+    }
+    Serial.println("Dados foram apagados");
+    ultimo_endereco = 0;
+    entrada_usuario = 0;
+  }
 }
--- pluviometro_V1/pluviometro_V1.ino
+++ pluviometro_V1/pluviometro_V1.ino
@@ -7,9 +7,9 @@ int sensor_mag = 3;
 int ler_sensor = 0;
 int ler_sensor_anterior = 0;
 int conta_virada_pote = 0;
-float ml_por_virada = 2.52;
+float ml_por_virada = 2.7;
 int tempo_segundos = 0;
-long periodo_segundos = 30;
+long periodo_segundos = 20;
 int ultimo_endereco = 0;
 
 
@@ -17,7 +17,7 @@ long tempsperiodo_segundos = 0;
 
 void calc_chuva() {
   m3_medido = conta_virada_pote * ml_por_virada;
-  mm_chuva = m3_medido / pow(10, 3);
+  mm_chuva = m3_medido * 127.39 / pow(10, 3);
   Serial.print("Viradas de copo: ");
   Serial.print(conta_virada_pote);
   Serial.print(" | Qtd de chuva: ");
@@ -37,12 +37,12 @@ void setup() {
 
 void loop() {
   ler_sensor = digitalRead(sensor_mag);
-  tempo_segundos = millis() / 1000;
+  
   if (ler_sensor != ler_sensor_anterior) {
     conta_virada_pote = conta_virada_pote + 1;
     pinMode(6, OUTPUT);
     digitalWrite(6, 82);
-    delay(0.5 * 1000);
+    delay(0.3* 1000);
     noTone(6);
     ler_sensor_anterior = ler_sensor;
     calc_chuva();
